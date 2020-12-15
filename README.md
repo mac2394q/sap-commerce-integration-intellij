6eac52f3be974d5cfc983b82f65e6d9bd2d236f8
Miguel Angel Claros Quintero <miguel.claros@ftstecnologia.com.co>
2020-12-15 18:57:53 -0500

    codebase v1.0

---
+++ README.md
@@ -0,0 +1,44 @@
+## SAP Commerce/Hybris Integration Intellij IDEA Plugin ##
+
+This plugin is a privacy and open-source focused fork of [Hybris Integration plugin](https://plugins.jetbrains.com/plugin/7525) by Alexander Bartash 
+who originally released it under GPL 3.0 license. If you want to support Alexander and use his relicensed closed source 
+version of the plugin, please visit the link above.
+
+# Install this plugin from IntelliJ IDEA https://plugins.jetbrains.com/plugin/12639-integration-for-sap-commerce #
+
+*Built plugin that can be loaded into IntelliJ IDEA can also be found in the releases section of this repository.*
+
+## Contributing ##
+
+Feel free to fork and modify this plugin as you wish. If you don't plan on maintaining this plugin yourself, consider 
+creating a pull request.
+
+I can not and would not want to change the license of this plugin so it will stay licensed under 
+LGPL v3.0.
+
+### Building ###
+
+You must have Java 8 installed.
+
+Run `./gradlew buildPlugin` to compile it. 
+
+Find the built zip file under `build/distributions/Integration-SAP-Commerce.zip`
+
+
+## Licence ##
+[GNU Lesser General Public License 3.0](http://www.gnu.org/licenses/)
+
+Copyright (C) 2014-2016 Alexander Bartash <AlexanderBartash@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
---
+++ src/com/intellij/idea/plugin/hybris/settings/HybrisDeveloperSpecificProjectSettingsComponent.java
@@ -0,0 +1,101 @@
+package com.intellij.idea.plugin.hybris.settings;
+
+import com.intellij.idea.plugin.hybris.common.HybrisConstants;
+import com.intellij.idea.plugin.hybris.common.services.CommonIdeaService;
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.project.Project;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static com.intellij.idea.plugin.hybris.common.HybrisConstants.HYBRIS_DEVELOPER_SPECIFIC_PROJECT_SETTINGS_COMPONENT_NAME;
+import static com.intellij.idea.plugin.hybris.common.HybrisConstants.HYBRIS_DEVELOPER_SPECIFIC_PROJECT_SETTINGS_FILE_NAME;
+
+@State(name = HYBRIS_DEVELOPER_SPECIFIC_PROJECT_SETTINGS_COMPONENT_NAME, storages = {@Storage(HYBRIS_DEVELOPER_SPECIFIC_PROJECT_SETTINGS_FILE_NAME)})
+public class HybrisDeveloperSpecificProjectSettingsComponent
+    implements PersistentStateComponent<HybrisDeveloperSpecificProjectSettings> {
+
+    private HybrisDeveloperSpecificProjectSettings state = new HybrisDeveloperSpecificProjectSettings();
+
+    public static HybrisDeveloperSpecificProjectSettingsComponent getInstance(@NotNull final Project project) {
+        return ServiceManager.getService(project, HybrisDeveloperSpecificProjectSettingsComponent.class);
+    }
+
+    @Nullable
+    @Override
+    public HybrisDeveloperSpecificProjectSettings getState() {
+        return state;
+    }
+
+    @Override
+    public void loadState(@NotNull final HybrisDeveloperSpecificProjectSettings state) {
+        XmlSerializerUtil.copyBean(state, this.state);
+    }
+
+    public HybrisRemoteConnectionSettings getActiveHybrisRemoteConnectionSettings(final Project project) {
+        final HybrisDeveloperSpecificProjectSettings state = getState();
+        if (state == null) {
+            return getDefaultHybrisRemoteConnectionSettings(project);
+        }
+        final List<HybrisRemoteConnectionSettings> connectionList = state.getRemoteConnectionSettingsList();
+        final List<HybrisRemoteConnectionSettings> remoteList = connectionList
+            .stream().filter(it -> it.getType() == HybrisRemoteConnectionSettings.Type.Hybris).collect(Collectors.toList());
+        if (remoteList.isEmpty()) {
+            return getDefaultHybrisRemoteConnectionSettings(project);
+        }
+        final String id = state.getActiveRemoteConnectionID();
+
+        return remoteList.stream().filter(e -> Objects.equals(id, e.getUuid())).findFirst().orElse(remoteList.get(0));
+    }
+
+    public HybrisRemoteConnectionSettings getActiveSolrConnectionSettings(final Project project) {
+        final HybrisDeveloperSpecificProjectSettings state = getState();
+        if (state == null) {
+            return getDefaultSolrRemoteConnectionSettings(project);
+        }
+        final List<HybrisRemoteConnectionSettings> connectionList = state.getRemoteConnectionSettingsList();
+        final List<HybrisRemoteConnectionSettings> solrList = connectionList
+            .stream().filter(it -> it.getType() == HybrisRemoteConnectionSettings.Type.SOLR).collect(Collectors.toList());
+        if (solrList.isEmpty()) {
+            return getDefaultSolrRemoteConnectionSettings(project);
+        }
+        final String id = state.getActiveSolrConnectionID();
+
+        return solrList.stream().filter(e -> Objects.equals(id, e.getUuid())).findFirst().orElse(solrList.get(0));
+    }
+
+    @NotNull
+    public HybrisRemoteConnectionSettings getDefaultHybrisRemoteConnectionSettings(final Project project) {
+        final HybrisRemoteConnectionSettings item = new HybrisRemoteConnectionSettings();
+        item.setType(HybrisRemoteConnectionSettings.Type.Hybris);
+        item.setUuid(UUID.randomUUID().toString());
+        item.setHostIP("localhost");
+        item.setPort(HybrisConstants.DEFAULT_TOMCAT_SSL_PORT);
+        item.setHacLogin("admin");
+        item.setHacPassword("nimda");
+        item.setGeneratedURL(CommonIdeaService.getInstance().getHostHacUrl(project, item));
+        return item;
+    }
+
+    @NotNull
+    public HybrisRemoteConnectionSettings getDefaultSolrRemoteConnectionSettings(final Project project) {
+        final HybrisRemoteConnectionSettings item = new HybrisRemoteConnectionSettings();
+        item.setType(HybrisRemoteConnectionSettings.Type.SOLR);
+        item.setUuid(UUID.randomUUID().toString());
+        item.setHostIP("localhost");
+        item.setPort(HybrisConstants.DEFAULT_SOLR_TOMCAT_SSL_PORT);
+        item.setSolrWebroot("solr");
+        item.setAdminLogin("solradmin");
+        item.setAdminPassword("admin123");
+        item.setGeneratedURL(CommonIdeaService.getInstance().getHostSolrUrl(project, item));
+        return item;
+    }
+}
